6b0741ae84d42a1db627c536d3e1baa256501e94
Wijayaac <wijayaac25@gmail.com>
2021-10-16 14:57:21 +0800

    ✅ two sums pointer

---
+++ app/problems/two-sum.js
@@ -0,0 +1,10 @@
+const twoSum = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  while (start < end) {
+    if (nums[start] + nums[end] > target) end--;
+    else if (nums[start] + nums[end] < target) start++;
+    else return [++start, ++end];
+  }
+  return [];
+};
