ef95c73f6defc3499741d520e52947002254dffb
Wijayaac <wijayaac25@gmail.com>
2021-12-05 09:28:28 +0800

    fix two sum probelms

--- app/problems/two-sum.js
+++ app/problems/two-sum.js
@@ -1,10 +1,12 @@
 const twoSum = function (nums, target) {
-  let start = 0;
-  let end = nums.length - 1;
-  while (start < end) {
-    if (nums[start] + nums[end] > target) end--;
-    else if (nums[start] + nums[end] < target) start++;
-    else return [++start, ++end];
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
   }
   return [];
 };
