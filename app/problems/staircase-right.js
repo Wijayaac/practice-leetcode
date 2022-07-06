604dbd1abfb387b90510e09031f02e1f0628d544
Wijayaac <wijayaac25@gmail.com>
2022-07-06 21:38:05 +0800

    Add : right staircase problem

---
+++ app/problems/staircase-right.js
@@ -0,0 +1,17 @@
+const staircase = (n) => {
+  let string = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i; j++) {
+      string += ` `;
+    }
+    for (let k = 1; k <= i; k++) {
+      string += `#`;
+    }
+    if (i !== n) {
+      string += "\n";
+    }
+  }
+  console.log(string);
+};
+
+module.exports = staircase;
