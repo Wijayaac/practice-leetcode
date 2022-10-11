e298109523e6a2051df4ff4f993c492c555835a1
Wijayaac <wijayaac25@gmail.com>
2022-10-12 05:42:16 +0800

    Add : array data structure

---
+++ app/data-structure/array.js
@@ -0,0 +1,17 @@
+// Array Data structure
+
+const arr = ['a','b','c','d']
+
+console.log(arr, 'one dimension')
+
+console.log(arr.length, 'array-length')
+
+const twoDimensionArray = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+console.log(twoDimensionArray, 'two dimension array')
+
+console.log(twoDimensionArray.length, 'length')
