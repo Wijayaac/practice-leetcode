b01e33e309a2dc659c757fc13e7fea238105952f
Wijayaac <wijayaac25@gmail.com>
2023-01-12 16:39:53 +0800

    Add : anagram problem with different approaches

--- app/problems/valid-anagram.js
+++ app/problems/valid-anagram.js
@@ -21,3 +21,32 @@ const isAnagram = (s, t) => {
   }
   return false;
 };
+
+function anagram(s, t) {
+  const count = {};
+  for (let char of s) {
+    if (!count[char]) {
+      count[char] = 0;
+    }
+    count[char]++;
+  }
+
+  for (let char of t) {
+    if (count[char] === undefined) {
+      return false;
+    } else {
+      count[char]--;
+    }
+  }
+
+  for (let char in count) {
+    if (count[char] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+let s = "anagram",
+  t = "nagaram";
+console.log(anagram(s, t));
