31d7a64127cf48e0151c11400de040dceed40f34
Wijayaac <wijayaac25@gmail.com>
2023-08-13 10:04:36 +0800

    Add : binary addition from geeksforgeeks site

---
+++ app/problems/add-binary-byte.js
@@ -0,0 +1,45 @@
+// Javascript program to add
+// two binary strings
+
+// This function adds two
+// binary strings and return
+// result as a third string
+function addBinary(a, b) {
+  // Initialize result
+  var result = "";
+
+  // Initialize digit sum
+  var s = 0;
+
+  // Traverse both strings starting
+  // from last characters
+  var i = a.length - 1,
+    j = b.length - 1;
+  while (i >= 0 || j >= 0 || s == 1) {
+    // Comput sum of last
+    // digits and carry
+    s += i >= 0 ? a.charAt(i).charCodeAt(0) - "0".charCodeAt(0) : 0;
+    s += j >= 0 ? b.charAt(j).charCodeAt(0) - "0".charCodeAt(0) : 0;
+
+    // If current digit sum is
+    // 1 or 3, add 1 to result
+    result = String.fromCharCode(parseInt(s % 2) + "0".charCodeAt(0)) + result;
+
+    // Compute carry
+    s = parseInt(s / 2);
+
+    // Move to next digits
+    i--;
+    j--;
+  }
+
+  return result;
+}
+
+//Driver code
+var a = "11",
+  b = "1";
+
+addBinary(a, b); //100
+
+// This code is contributed by Amit Katiyar
